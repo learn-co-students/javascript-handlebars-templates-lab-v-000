b5a114e07f2ed00099ade78752d0c6023cb7c514
Scott Savage <snsavage@gmail.com>
2017-03-31 17:11:42 -0400

    Pass all specs except for editing

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
 function init() {
-  var recipeFormTemplate = Handlebars
-    .compile(document.getElementById("recipe-form-template").innerHTML);
-  document.getElementsByTagName("main")[0].innerHTML += recipeFormTemplate();
+  let recipeDetailsPartial = document.
+    getElementById('recipe-details-partial').innerHTML;
+  let recipeFormPartial = document.
+    getElementById('recipe-form-partial').innerHTML;
+
+  Handlebars.registerPartial('recipeDetailsPartial', recipeDetailsPartial);
+  Handlebars.registerPartial('recipeFormPartial', recipeFormPartial);
+
+  Handlebars.registerHelper('displayIngredient', function () {
+    return new Handlebars.SafeString(this.ingredient);
+  })
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
+
+  let recipeFormTemplate = Handlebars
+    .compile(document.getElementById("recipe-form-template").innerHTML);
+
+  document.getElementById('main').innerHTML = recipeFormTemplate();
 })
+
+function createRecipe() {
+  event.preventDefault();
+
+  let recipe = {
+    name: document.getElementById('name').value,
+    description: document.getElementById('description').value,
+    ingredients: ingredientValues(document.getElementsByName('ingredients')),
+  }
+
+  let template = Handlebars
+    .compile(document.getElementById('recipe-template').innerHTML);
+
+  let result = template(recipe);
+
+  document.getElementById('main').innerHTML = result;
+}
+
+function displayEditForm() {
+}
+
+function ingredientValues(ingredients) {
+  let result = [];
+
+  ingredients.forEach((currentValue) => {
+    result.push({ingredient: currentValue.value});
+  });
+
+  return result;
+}
