5a555c09a36635618373e845b24f8b2e5ca2df05
Scott Savage <snsavage@gmail.com>
2017-04-01 11:12:18 -0400

    Pass all lab specs

--- index.js
+++ index.js
@@ -10,24 +10,24 @@ function init() {
   Handlebars.registerHelper('displayIngredient', function () {
     return new Handlebars.SafeString(this.ingredient);
   })
-}
-
-document.addEventListener("DOMContentLoaded", function(event) {
-  init()
 
   let recipeFormTemplate = Handlebars
     .compile(document.getElementById("recipe-form-template").innerHTML);
 
   document.getElementById('main').innerHTML = recipeFormTemplate();
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  init()
 })
 
 function createRecipe() {
-  event.preventDefault();
+  // event.preventDefault();
 
   let recipe = {
     name: document.getElementById('name').value,
     description: document.getElementById('description').value,
-    ingredients: ingredientValues(document.getElementsByName('ingredients')),
+    ingredients: ingredientFieldValues(document.getElementsByName('ingredients')),
   }
 
   let template = Handlebars
@@ -39,14 +39,48 @@ function createRecipe() {
 }
 
 function displayEditForm() {
+  // event.preventDefault();
+
+  let recipe = {
+    name: document.getElementById('name').innerHTML,
+    description: document.getElementById('description').innerHTML,
+    ingredients: ingredientValues(document.querySelectorAll('.ingredients')),
+    onSubmit: "updateRecipe();"
+  }
+
+  let recipeFormTemplate = Handlebars
+    .compile(document.getElementById("recipe-form-template").innerHTML);
+
+  document.getElementById('main').innerHTML = recipeFormTemplate(recipe);
+}
+
+function updateRecipe() {
+  let recipe = {
+    name: document.getElementById('name').value,
+    description: document.getElementById('description').value,
+    ingredients: ingredientFieldValues(document.getElementsByName('ingredients')),
+  }
+
+  let template = Handlebars
+    .compile(document.getElementById('recipe-template').innerHTML);
+
+  let result = template(recipe);
+
+  document.getElementById('main').innerHTML = result;
 }
 
 function ingredientValues(ingredients) {
   let result = [];
 
-  ingredients.forEach((currentValue) => {
-    result.push({ingredient: currentValue.value});
-  });
+  [...ingredients].forEach(e => result.push({ingredient: e.innerHTML}));
+
+  return result;
+}
+
+function ingredientFieldValues(ingredients, method) {
+  let result = [];
+
+  [...ingredients].forEach(e => result.push({ingredient: e[method]}));
 
   return result;
 }
