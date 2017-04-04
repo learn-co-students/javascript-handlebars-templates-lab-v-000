0f7befa03f09f0eeadbcb69f4b1fde7b4da513ca
mcclains2003 <mcclains2003@gmail.com>
2017-04-04 17:09:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,73 @@
+function initForm() {
+  var formTemplate = document.getElementById("recipe-form-template").innerHTML
+  var template = Handlebars.compile(formTemplate)
+  document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
+}
+
+function createRecipe() {
+  var recipe = getRecipeIngredients()
+  var recipeTemplate = document.getElementById("recipe-template").innerHTML
+  var template = Handlebars.compile(recipeTemplate)
+  document.getElementById("main").innerHTML = template(recipe)
+}
+
+function updateRecipe() {
+  var recipe = getRecipeIngredients()
+  var recipeTemplate = document.getElementById("recipe-template").innerHTML
+  var template = Handlebars.compile(recipeTemplate)
+  document.getElementById("main").innerHTML = template(recipe)
+}
+
+// 
+function displayEditForm() {
+  var name = document.getElementById("recipeTitle").innerText
+  var description = document.getElementById("recipeDetailsDescription").innerText
+  var ingredientsNodes = document.getElementsByName("ingredientsList")
+  var ingredients = []
+  for(var i=0;i<ingredientsNodes.length;i++) {
+    ingredients.push(ingredientsNodes[i].innerText)
+  }
+
+  var recipe = {name, description, ingredients, submitAction: 'createRecipe()'}
+
+  var recipeFormTemplate = document.getElementById("recipe-form-template").innerHTML
+  var template = Handlebars.compile(recipeFormTemplate)
+  document.getElementById("main").innerHTML = template(recipe)
+}
+
+// Function takes the ingredients that have the element name of "ingredients" and sets them to a variable, then assigns a blank array to a variable also, iterate through each ingredient while making sure only non-blank ones are pushed into the new array. We then retrieve the name value and description value. Create an object called recipe that holds all the items we need for our functions when creating or editing a recipe.
+function getRecipeIngredients() {
+  var ingredientsNodes = document.getElementsByName("ingredients")
+  var ingredients = []
+  for(var i=0;i<ingredientsNodes.length;i++) {
+    if(ingredientsNodes[i].value !== "") {
+      ingredients.push(ingredientsNodes[i].value)
+    }
+  }
+  var name = document.getElementById("recipeName").value
+  var description = document.getElementById("recipeDescription").value
+  var recipe = {name, ingredients, description}
+  return(recipe)
+}
+
+// displayIngredient helper method that just takes each ingredient and puts them in a list
+// registers both partials
+function handlebarsSetup() {
+  //put any handlebars registrations here.
+  Handlebars.registerHelper('displayIngredient', function(ingredient) {
+    return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
+  })
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
+}
+
+
 function init() {
   //put any page initialization/handlebars initialization here
+  handlebarsSetup()
+  initForm()
 }
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
