c7bab15a82bd44cb85669d146b6d182750977059
walwoodr <walwoodr@gmail.com>
2017-04-12 12:16:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 function init() {
-  //put any page initialization/handlebars initialization here
-}
+  var template = Handlebars.compile(document.getElementById('recipe-form-template').innerHTML);
+  var result = template();
+  var main = document.getElementById('main');
+  main.innerHTML += result;
+};
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
-})
+});
+
+Handlebars.registerPartial('recipeDetailsPartial', document.getElementById('recipe-details-partial').innerHTML);
+
+Handlebars.registerHelper('displayIngredient', function() {
+    if (this.value) {
+      return new Handlebars.SafeString("<li>" + this.value + "</li>")
+    }
+});
+
+function createRecipe() {
+  var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+  var result = template({
+    'name': document.getElementById('name').value,
+    'description': document.getElementsByName('description')[0].value,
+    'ingredients': document.getElementsByName('ingredients')
+  })
+  document.getElementById('main').innerHTML += result;
+}
