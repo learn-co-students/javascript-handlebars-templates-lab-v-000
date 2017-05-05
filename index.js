d768763917117c3b547f8d1aa57ff29f7b6df8e8
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-05-05 11:32:57 -0400

    Stumped.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function init() {
     renderForm();
     Handlebars.registerPartial('recipeDetailsPartial', document.getElementById('recipe-details-partial'));
-    Handlebars.registerHelper('displayIngredient', displayIngredient());
+    Handlebars.registerHelper('displayIngredient', function(ingredient){
+//        debugger
+        return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
+    });
 }
 document.addEventListener("DOMContentLoaded", function(event) {
     init()
@@ -12,23 +15,27 @@ function renderForm() {
     document.getElementById('main').innerHTML += form_template();
 }
 
-function displayIngredient() {
-    alert(this)
-}
+
 
 function createRecipe(){
     
-    const name = document.getElementsByName('name')[0].value
+    const name = document.getElementsByName('name')[0].value.toString();
     const description = document.getElementsByName('description')[0].value
     let ingredient_list = document.getElementsByName('ingredients')
     ingredient_list = Array.prototype.slice.call(ingredient_list)
     
     const ingredients = []
           ingredient_list.forEach(function(ingredient){
-        ingredients.push({name:ingredient.value})
+        ingredients.push(ingredient.value)
     })
+    
+    const recipe_template_string = document.getElementById('recipe-template').innerHTML;
+    const recipe_template = Handlebars.compile(recipe_template_string);
+    const recipe = 'name'//, description:description, ingredients:ingredients};
     debugger
-    const recipe_template = Handlebars.compile(document.getElementById('recipe-template').innerHTML);
-    document.getElementById('main').innerHTML += recipe_template({name:name, description:description, ingredients:ingredients});
-
+    const recipe_view = recipe_template(recipe);
+    document.getElementById('main').innerHTML += "Hello World"; //recipe_template(recipe);
+    
 }
+
+function displayEditForm(){}
