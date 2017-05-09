0e09631af86402009764e5fbe7934a02a19fec6a
Ray Mulligan <rmullig2@optonline.net>
2017-05-09 15:42:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('recipeDetailsPartial', '{{name}}')
+  Handlebars.registerPartial('recipeFormPartial', '{{name}}')
+  Handlebars.registerHelper('displayIngredient', function() {
+  return document.getElementById("recipe-form-template").ingredients[ingredient]
+  })
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+ 
+function createRecipe() {
+  var recipe = {
+    name: document.getElementsByName("name")[0],
+    description: document.getElementsByName("description")[0],
+    ingredients: document.getElementsByName("ingredients")
+  }
+  displayEditForm();
+  return document.getElementsByTagName("main")[0].innerHTML += result;
+}
+
+function displayEditForm() {
+  return Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+}
+
+function updateRecipe() {
+  //code
+}
+
