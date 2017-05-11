2f4a71fbd070c94a269320d6b97f906f2dea4453
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-05-11 12:13:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function init() {
   //put any page initialization/handlebars initialization here
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById('recipe-details-partial').innerHTML)
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById('recipe-form-partial').innerHTML)
   Handlebars.registerHelper('displayIngredient', function () {
-    return new Handlebars.safeString("test")
+    return new Handlebars.SafeString(this.value)
   })
 }
 document.addEventListener("DOMContentLoaded", function(event) {
@@ -16,9 +17,15 @@ function recipeFormTemplate() {
 
 function createRecipe() {
  var ingredients = document.getElementsByName("ingredients")
- var name = document.getElementById('name')
- var description = document.getElementById('description')
+ var name = document.getElementById('name').value
+ var description = document.getElementById('description').value
 
  var template = Handlebars.compile(document.getElementById('recipe-template').innerHTML)
  var html = template({'ingredients': ingredients, 'description': description, 'name': name})
+
+ document.querySelector("main").innerHTML += html
+}
+
+function displayEditForm() {
+
 }
