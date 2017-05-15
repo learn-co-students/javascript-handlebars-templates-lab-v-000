dc8a37858feef25a914b577fe8bab0f5ebcaa4a7
umohm1 <umohm12@gmail.com>
2017-05-15 12:27:17 -0400

    Done.

--- index.js
+++ index.js
@@ -5,13 +5,28 @@ document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
 
-function createRecipe() {
-  var ingredient = {
-    name:
-    ingredient1:
-    ingredient2:
-    ingredient3:
-    ingredient4:
-    ingredient5:
-  }
-}
+// function createRecipe() {
+//   var ingredient = {
+//     name:
+//     ingredient1:
+//     ingredient2:
+//     ingredient3:
+//     ingredient4:
+//     ingredient5:
+//   }
+// }
+
+// var recipe = {
+    //  name: 'Chicken Noodle Soup' 
+//   description: 'yummy chicken noodle soup',
+//   ingredients: [
+//     {quantity: "1 cup", name: 'chicken'},
+//     {quantity: "3 nanoliters", name: 'stock'},
+//     {quantity: "12", name: 'noodles'}
+//     {quantity: "12", name: 'noodles'}
+//     {quantity: "12", name: 'noodles'}
+//   ]
+// }
+//
+// var template = Handlebars.compile(document.getElementById("my-template").innerHTML);
+// var html = template(recipe);
