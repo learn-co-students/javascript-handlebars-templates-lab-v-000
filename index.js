af0ca610a22df6b278874923b3d5203477bd2371
Omar Mason <mr.omarmason@gmail.com>
2017-05-31 19:24:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 function init() {
-  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
-  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
-  Handlebars.registerHelper('displayIngredient', function(ingredient) {
-  return new Handlebars.SafeString(ingredient)
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML);
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
+  Handlebars.registerHelper('displayIngredient', function(ingredients) {
+  return new Handlebars.SafeString(ingredients)
+  });
+  var formTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+  document.getElementsByTagName('main')[0].innerHTML = formTemplate({'submitAction': 'createRecipe()'});
+};
+
+document.addEventListener('DOMContentLoaded', function(event){
+  init();
+});
+
+function recipeAttrs() {
+  var name = document.getElementsByName("name").value;
+	var description = document.getElementsByName("description").value;
+  var ingredients = Array.prototype.map.call(document.getElementsByName('ingredients'), (ingredient) => {
+    return ingredient.value
+  })
+	return {name, description, ingredients};
 }
 
-var recipe = {
-  description: 'yummy chicken noodle soup',
-  ingredients: [
-    {quantity: "1 cup", name: 'chicken'},
-    {quantity: "3 nanoliters", name: 'stock'},
-    {quantity: "12", name: 'noodles'}
-  ]
+function createRecipe() {
+  var recipe = recipeAttrs();
+  var recipeTemplate = Handlebars.compile(document.getElementById('recipe-template').innerHTML);
+  document.getElementsByTagName('main').innerHTML += recipeTemplate(recipe);
 }
 
-Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
-function renderMain() {
-  var template = Handlebars.compile(document.getElementById("main-template").innerHTML);
-  var html = template({name: 'Gordon Ramsay'});
+function displayEditForm () {
+  var name = document.getElementsByName("name").value;
+	var description = document.getElementsByName("description").value;
+  var ingredients = Array.prototype.map.call(document.getElementsByName('ingredients'), (ingredient) => {
+    return ingredient.value
+  })
+	var recipe = {name, description, ingredients, submitAction: 'updateRecipe()'}
+	var recipeTemplateEdit = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+	document.getElementById("main").innerHTML = recipeTemplateEdit(recipe);
 }
 
-Handlebars.registerHelper('displayIngredient', function(ingt) {
-  return new Handlebars.SafeString(<p> ingt.quantity, ingt.name </p>)
-})
+function updateRecipe() {
+  var recipe = recipeAttrs()
+  var editRecipeTemplate = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+  document.getElementById("main").innerHTML += editRecipeTemplate(recipe)
+}
