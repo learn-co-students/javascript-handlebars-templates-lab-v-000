9148a9290ae5e4c13054996a29f68dfec8f37650
Omar Mason <mr.omarmason@gmail.com>
2017-05-31 06:13:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 function init() {
-  //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  Handlebars.registerHelper('displayIngredient', function(ingredient) {
+  return new Handlebars.SafeString(ingredient)
 }
-document.addEventListener("DOMContentLoaded", function(event) {
-  init()
+
+var recipe = {
+  description: 'yummy chicken noodle soup',
+  ingredients: [
+    {quantity: "1 cup", name: 'chicken'},
+    {quantity: "3 nanoliters", name: 'stock'},
+    {quantity: "12", name: 'noodles'}
+  ]
+}
+
+Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+function renderMain() {
+  var template = Handlebars.compile(document.getElementById("main-template").innerHTML);
+  var html = template({name: 'Gordon Ramsay'});
+}
+
+Handlebars.registerHelper('displayIngredient', function(ingt) {
+  return new Handlebars.SafeString(<p> ingt.quantity, ingt.name </p>)
 })
