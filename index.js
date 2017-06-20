4f05bf4ed786556d16fb9cdd08e3ab95d6c01653
Mary <mumoh@mozilla.com>
2017-06-20 17:56:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,32 +1,21 @@
 function init() {
-  //put any page initialization/handlebars initialization here
-}
+   // var formTemplate = document.getElementById("recipe-form-template").innerHTML
+   // var template = Handlebars.compile(formTemplate)
+   // document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
+ }
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
 
-// function createRecipe() {
-//   var ingredient = {
-//     name:
-//     ingredient1:
-//     ingredient2:
-//     ingredient3:
-//     ingredient4:
-//     ingredient5:
-//   }
+// var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+// var html = template(recipe);
+// document.getElementsByTagName("main")[0].innerHTML += html;
 // }
 
-// var recipe = {
-    //  name: 'Chicken Noodle Soup' 
-//   description: 'yummy chicken noodle soup',
-//   ingredients: [
-//     {quantity: "1 cup", name: 'chicken'},
-//     {quantity: "3 nanoliters", name: 'stock'},
-//     {quantity: "12", name: 'noodles'}
-//     {quantity: "12", name: 'noodles'}
-//     {quantity: "12", name: 'noodles'}
-//   ]
-// }
-//
-// var template = Handlebars.compile(document.getElementById("my-template").innerHTML);
-// var html = template(recipe);
+function createRecipe() {
+    var recipe = getRecipeVals()
+    var recipeTemplate = document.getElementById("recipe-template").innerHTML
+    var template = Handlebars.compile(recipeTemplate)
+    document.getElementById("main").innerHTML = template(recipe)
+}
