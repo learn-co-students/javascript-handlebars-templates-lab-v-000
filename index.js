793a9fc5138bfe36ea617a0ecbe792e6282aee1b
jessica fletcher <cara.kane@gmail.com>
2017-08-31 19:00:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 function init() {
   //put any page initialization/handlebars initialization here
-  function createRecipe() {
-      var recipe = {
-          name: document.getElementById("recipeName").value,
-          description: document.getElementById("recipeDescription").value,
-          ingredients: document.getElementsByName("recipeIngredient").value
-      }
-      var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
-      var result = template(recipe);
-      document.getElementsByTagName("main")[0].innerHTML += result;
-  }
+
 
 
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-partial-template").innerHTML)
@@ -17,3 +8,15 @@ function init() {
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+
+function createRecipe() {
+    var recipe = {
+        name: document.getElementById("recipeName").value,
+        description: document.getElementById("recipeDescription").value,
+        ingredients: document.getElementsByName("recipeIngredient").value
+    }
+    var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+    var result = template(recipe);
+    document.getElementsByTagName("main")[0].innerHTML += result;
+}
