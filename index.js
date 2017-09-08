0ddae45ef0cfc61d1d5a955290461245c9241f42
hansenjl <jenn.leigh.hansen@gmail.com>
2017-09-08 11:29:22 -0700

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,89 @@
 function init() {
-  //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
+
+
+  Handlebars.registerHelper('displayIngredient', function(ingredient){
+    return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
+  })
+
+  var submitAction = {'submitAction': 'createRecipe()'}
+
+  var mainTemplate =  Handlebars.compile(document.getElementById("recipe-form-template").innerHTML)
+
+  document.getElementsByTagName("main")[0].innerHTML = mainTemplate(submitAction)
 }
+
+
+
+function createRecipe(){
+  var recipe = recipeVariables()
+
+  var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML)
+
+  var html = template(recipe)
+
+  document.getElementsByTagName("main")[0].innerHTML = html
+}
+
+function recipeVariables(){
+  var ingredientEntries = document.getElementsByName("ingredients")
+  var ingredientsArray = []
+  for (var i = 0; i < ingredientEntries.length; i++) {
+    if (ingredientEntries[i].value !== ""){
+      ingredientsArray.push(ingredientEntries[i].value)
+    }
+  }
+
+  var recipe = {
+    name: document.getElementsByName("name").value,
+    description: document.getElementsByName("description").value,
+    ingredients: ingredientsArray
+  }
+  return recipe
+}
+
+function displayEditForm(){
+  var ingredientEntries = document.getElementsByName("ingredients")
+  var ingredientsArray = []
+  for (var i = 0; i < ingredientEntries.length; i++) {
+    if (ingredientEntries[i].value !== ""){
+      ingredientsArray.push(ingredientEntries[i].value)
+    }
+  }
+
+
+  var recipe = {
+    name: document.getElementsByName("name").value,
+    description: document.getElementsByName("description").value,
+    ingredients: ingredientsArray,
+    submitAction: 'updateRecipe()'
+  }
+
+  var template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML)
+
+  var html = template(recipe)
+
+  document.getElementsByTagName("main")[0].innerHTML = html
+}
+
+function updateRecipe(){
+  var recipe = recipeVariables()
+
+  var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML)
+
+  var html = template(recipe)
+
+  document.getElementsByTagName("main")[0].innerHTML = html
+
+}
+
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
