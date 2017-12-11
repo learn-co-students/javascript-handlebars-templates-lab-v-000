2a781291340513018d6c7daf3613739f2ba92c2a
John Tuttle <thejwt@gmail.com>
2017-12-11 04:00:58 +0000

    finished createRecipe

--- index.js
+++ index.js
@@ -2,7 +2,22 @@ function createRecipe() {
   var name = document.getElementById("name").value;
   var description = document.getElementById("description").value;
 
-  
+  var ingredients = document.getElementsByName("ingredients")
+
+  var recipeFormTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+  var rfTemplate = recipeFormTemplate();
+
+  var recipe = {
+    name: name,
+    description: description,
+    ingredients: ingredients
+  }
+
+  var recipeTemplate = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+  var rTemplate = recipeTemplate(recipe);
+
+  document.getElementById("main").innerHTML += rTemplate;
+  document.getElementById("main").innerHTML += rfTemplate;
 }
 
 function init() {
