1762cd020b35d2278e8c21aa85b4b5f6655e4b59
John Tuttle <thejwt@gmail.com>
2017-12-11 00:47:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+function createRecipe() {
+  var name = document.getElementById("name").value;
+  var description = document.getElementById("description").value;
+
+  
+}
+
 function init() {
   //put any page initialization/handlebars initialization here
+  var recipeFormTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+  var rfTemplate = recipeFormTemplate();
+  document.getElementById("main").innerHTML += rfTemplate;
 }
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
