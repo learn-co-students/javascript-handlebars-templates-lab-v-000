b5ea1b9b845fad64b05bcf90b1b62b4fc2bbc235
Fachastorm <rodrigo.lodeiro@live.com>
2018-03-01 13:51:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
+function createRecipe() {
+  var ingredients = document.getElementsByName("ingredients").value;
+  var description = document.getElementById("description").value;
+  var name = document.getElementById("name").value;
+  var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+  var html = template(ingredients);
+  document.getElementsByTagName("main")[0].innerHTML += html;
+}
+
+function updateRecipe() {
+  var ingredients = document.getElementsByName("ingredients").value;
+  var description = document.getElementById("description").value;
+  var name = document.getElementById("name").value;
+  var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+  var html = template(ingredients);
+  document.getElementsByTagName("main")[0].innerHTML += html;
+}
+
+function displayEditForm() {
+  var template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+  document.getElementsByTagName("main")[0].innerHTML += template;
+}
+
 function init() {
   //put any page initialization/handlebars initialization here
+
+  var recipe = {
+  description: 'yummy chicken noodle soup',
+  ingredients: [
+    {quantity: "1 cup", name: 'chicken'},
+    {quantity: "3 nanoliters", name: 'stock'},
+    {quantity: "12", name: 'noodles'}
+  ]
 }
+
+  Handlebars.registerHelper('displayIngredient', function() {
+    if(this.name === "ingredients") {
+      return new Handlebars.SafeString(this.name)
+    }
+  });
+
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById('recipe-details-partial').innerHTML);
+
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById('recipe-template').innerHTML);
+
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
