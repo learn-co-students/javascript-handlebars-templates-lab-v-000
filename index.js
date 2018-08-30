a62162a5ed1c77d8f0a782a50ac436e6d0e36e68
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2018-08-30 22:41:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
 function init() {
   //put any page initialization/handlebars initialization here
+
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById('recipe-details-partial').innerHTML);
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById('recipe-form-partial').innerHTML);
+  Handlebars.registerHelper('displayIngredient', (ingredient) => {
+    return new Handlebars.SafeString(
+	    '<ol>'+ingredient.value+'</ol>'
+	    );
+  });
+  
+  var formInitializer = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML); 
+  document.getElementById("main").innerHTML = formInitializer({'createRecipe':'createRecipe()'}); 
+
 }
-document.addEventListener("DOMContentLoaded", function(event) {
-  init()
-})
+
+function createRecipe() {
+	
+	var context = {
+		name: document.getElementById("name").value,
+		description: document.getElementById("description").value,
+		ingredients: document.getElementsByName('ingredients')
+	}
+	
+	var template = Handlebars.compile(document.getElementById('recipe-template').innerHTML);
+	document.getElementById('main').innerHTML = template(context);
+
+}
+
+function displayEditForm() {
+	debugger
+  var context = {
+	  'createRecipe': 'updateRecipe()',
+	  name: document.getElementById('recipe-name').innerHTML,
+	  description: document.getElementById('recipe-description').innerHTML,
+	  ingredients: document.getElementsByTagName('ol')
+  }
+
+  var editForm = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+
+  document.getElementById('main').innerHTML = editForm(context);
+}
+
+function updateRecipe() {
+	createRecipe()
+}
+
+	document.addEventListener( "DOMContentLoaded", function(event) { 
+		init() 
+	})
